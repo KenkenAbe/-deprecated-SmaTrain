040ab506a56341c2f7d55d4067b46c88b12714bd
kenelaoy <30878285+kenelaoy@users.noreply.github.com>
2017-11-27 17:33:09 +0900

    全線駅名取得と保存の処理を追加

--- SmaTrain/AppDelegate.swift
+++ SmaTrain/AppDelegate.swift
@@ -7,6 +7,55 @@
 //
 
 import UIKit
+import Realm
+import RealmSwift
+
+class RegisterStationAndDistrict: Object{
+    @objc dynamic var StationName = ""
+    @objc dynamic var StationCode = ""
+    @objc dynamic var LineNameInStation = ""
+    @objc dynamic var LineCodeInStation = ""
+    @objc dynamic var TrainDirection = ""
+}
+
+class LineData:Object{
+    @objc dynamic var dataID = 0
+    @objc dynamic var OperatorName = "" //運行会社
+    @objc dynamic var lineName = "" //路線名
+    @objc dynamic var isLtdEXP = false //特急（JR線の特急は遅延時分を表示しない）
+    @objc dynamic var lineID = 0 //路線ID
+    @objc dynamic var lineCode = "" //路線コード（東京メトロでリクエストを投げる際に使用するためのコード）
+    @objc dynamic var lineColor = "" //路線カラー（HTMLカラー)
+    @objc dynamic var isUnique = false //JREの首都圏列車位置情報とは違う法則性のURLを使っているか
+    @objc dynamic var dataURL = "" //isUniqueがtrueの場合はURLを記録
+    @objc dynamic var type = "" //データタイプ（例：JRE/京王->HTML 東急/東京メトロ->JSON）
+}
+
+class stationData:Object{
+    @objc dynamic var operatorName = "" //運行会社（例：東京メトロ→"TokyoMetro" 東急電鉄→"Tokyu"）
+    @objc dynamic var lineCode = "" //該当駅の路線コード（複数路線が乗り入れる駅も路線ごとに独立して登録）
+    @objc dynamic var stationID = 0 //路線内でのナンバリング（例：半蔵門線渋谷 -> Z01 -> 1）
+    @objc dynamic var stationName = "" //駅名
+    @objc dynamic var stationCode = "" //API内で使用されている駅名
+    @objc dynamic var bothStation = false //駅間か（東急が駅間も駅として情報を持っているため）（メトロは基本false）
+    @objc dynamic var stationColor = ""
+}
+
+class timeTableInTrainInStation:Object{
+    @objc dynamic var operationgCode = "" //運行番号
+    @objc dynamic var lineCode = "" //東京メトロ線のAPIで使用される路線名
+    @objc dynamic var trainID = 0 //始発から順に数えた時のID（列車１本に１コード）
+    @objc dynamic var trainDirection = "" //電車の方向
+    @objc dynamic var stationCode = "" //駅名
+    @objc dynamic var stationID = 0
+    @objc dynamic var time = "" //発車時刻
+    @objc dynamic var week = "" //平日・休日・土曜の種別
+}
+
+
+class AppMetaData:Object{
+    @objc dynamic var isFirstLaunch = false
+}
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +65,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        /*let userDefault = UserDefaults.standard
+        let dict = ["isFirstLaunch":true]
+        
+        userDefault.register(defaults: dict)
+        if userDefault.bool(forKey: "firstLaunch") {
+            userDefault.set(false, forKey: "firstLaunch")
+            let database = try! Realm()
+            let objects = AppMetaData()
+            
+            objects.isFirstLaunch = true
+            
+            try! database.write {
+                database.add(objects)
+            }
+        }
+        */
         return true
     }
 
--- SmaTrain/ViewController.swift
+++ SmaTrain/ViewController.swift
@@ -7,19 +7,146 @@
 //
 
 import UIKit
+import Alamofire
+import Realm
+import RealmSwift
+import SwiftyJSON
 
-class ViewController: UIViewController {
-
+class TrainTableViewController: UIViewController,UIScrollViewDelegate {
+    
+    let TokyoMetroAccessToken = "12e6c4c1e608511e3dcf26f416d861e261d8efa412992708210f46ba1005161f"
+    let tokyuURL = "https://tokyu-tid.s3.amazonaws.com/dento.json"
+    
+    @IBOutlet var MainView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let json = JsonGet(fileName: "line")
+        
+        let lineDic = json.dictionaryObject
+        
+        let lineKeys:Array = Array(lineDic!.keys)
+        
+        print(lineKeys)
+        
+        for i in 0...lineKeys.count-1{
+            print("\(lineKeys[i])の路線コードは\(String(describing:json[lineKeys[i]]))です")
+        }
+        
+        stationInit()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func stationInit(){
+        let url = URL(string:"https://api.tokyometroapp.jp/api/v2/datapoints?rdf:type=odpt:Station&acl:consumerKey=\(self.TokyoMetroAccessToken)")
+        
+        Alamofire.request(url!).responseJSON{response in
+            if response.result.value != nil{
+                let database = try! Realm()
+                
+                try! database.write {
+                    database.deleteAll()
+                }
+                let jsonDict = JSON(response.result.value!)
+                for i in 0...jsonDict.count-1{
+                    print("\(String(describing:jsonDict[i]["odpt:railway"]))線-\(String(describing:jsonDict[i]["dc:title"]))駅")
+                    let object = stationData()
+                    object.lineCode = String(describing:jsonDict[i]["odpt:railway"])
+                    object.stationName = String(describing:jsonDict[i]["dc:title"])
+                    object.operatorName = "TokyoMetro"
+                    var stationIDRaw = String(describing:jsonDict[i]["odpt:stationCode"])
+                    stationIDRaw.remove(at: stationIDRaw.startIndex) //路線IDから路線記号を除く（例：半蔵門線渋谷-> Z01 -> 01）
+                    object.stationCode = String(describing:jsonDict[i]["owl:sameAs"])
+                    object.stationID = Int(stationIDRaw)!
+                    
+                    let database = try! Realm()
+                    
+                    try! database.write {
+                        database.add(object)
+                    }
+                }
+                //print(database.objects(stationData.self))
+                
+                let HanzomonLineCode = "odpt.Railway:TokyoMetro.Ginza"
+                
+                let HanzomonLine = database.objects(stationData.self).filter("lineCode == %@",HanzomonLineCode).sorted(byKeyPath: "stationID", ascending: true)
+                for j in 0...HanzomonLine.count-1{
+                    print(HanzomonLine[j].stationName)
+                }
+            }
+            
+            
+            
+        }
+    }
 
 
+    func JsonGet(fileName :String) -> JSON {
+        let path = Bundle.main.path(forResource: fileName, ofType: "json")
+        print(path)
+        
+        do{
+            let jsonStr = try String(contentsOfFile: path!)
+            print(jsonStr)
+            
+            let json = JSON.parse(jsonStr)
+            
+            return json
+        } catch {
+            return nil
+        }
+        
+    }
 }
 
+class FirstConfigurationController: UIViewController{
+    let TokyoMetroAccessToken = "12e6c4c1e608511e3dcf26f416d861e261d8efa412992708210f46ba1005161f"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let json = JsonGet(fileName: "line")
+        
+        print(json[0].dictionaryValue)
+        stationInit()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    func stationInit(){
+        let url = URL(string:"https://api.tokyometroapp.jp/api/v2/datapoints?rdf:type=odpt:Station&acl:consumerKey=\(self.TokyoMetroAccessToken)")
+        
+        Alamofire.request(url!).responseJSON{response in
+            if response.result.value != nil{
+                let jsonDict = JSON(response.result.value!)
+                for i in 0...jsonDict.count-1{
+                    print("\(String(describing:jsonDict[i]["odpt:railway"]))線-\(String(describing:jsonDict[i]["dc:title"]))駅")
+                }
+            }
+        }
+    }
+    
+    func JsonGet(fileName :String) -> JSON {
+        let path = Bundle.main.path(forResource: fileName, ofType: "json")
+        print(path)
+        
+        do{
+            let jsonStr = try String(contentsOfFile: path!)
+            print(jsonStr)
+            
+            let json = JSON.parse(jsonStr)
+            
+            return json
+        } catch {
+            return nil
+        }
+        
+    }
+}
